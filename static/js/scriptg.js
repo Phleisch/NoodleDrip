a53a9ce988c6c4c7ccbd67c3cc88e7008e3cd4b0
phleisch <KaiFleischmanS@gmail.com>
2019-04-06 15:07:45 -0500

    Added new font

--- static/js/scriptg.js
+++ static/js/scriptg.js
@@ -1,7 +1,7 @@
-var colors = ["#FFFF00", "#E6FB04", "#FF0000", "#FD1C03", "#FF3300", "#FF6600", "#00FF00", "#00FF33", "#00FF66", "#33FF00 ", "#00FFFF", "#099FFF", "#0062FF", "#0033FF", "#FF00FF", "#FF00CC", "#FF0099", "#CC00FF", "#9D00FF", "#CC00FF", "#6E0DD0", "#9900FF", "#FFFFFF"];
+var colors = ["#FFFF00", "#E6FB04", "#FF0000", "#FD1C03", "#FF3300", "#FF6600", "#00FF00", "#00FF33", "#00FF66", "#33FF00 ", "#00FFFF", "#099FFF", "#0062FF", "#0033FF", "#FF00FF", "#FF00CC", "#FF0099", "#CC00FF", "#9D00FF", "#CC00FF", "#6E0DD0", "#9900FF"];
 
 function setColor() {
-    var randNum = Math.floor(Math.random() * 20);
+    var randNum = Math.floor(Math.random() * 22);
     var header = document.getElementsByTagName("header");
     header[0].style.borderBottomColor = colors[randNum];
 }
